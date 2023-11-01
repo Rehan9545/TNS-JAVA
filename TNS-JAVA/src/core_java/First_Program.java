509463d79f4adfb6aa24ad1336b4b2657ba58ea8
rehan <rehanfga@gmail.com>
2023-11-01 18:02:27 +0530

    secound Commit

--- TNS-JAVA/src/core_java/First_Program.java
+++ TNS-JAVA/src/core_java/First_Program.java
@@ -1,5 +1,5 @@
 package core_java;
-
+import java.util.*;
 public class First_Program {
 
 	public First_Program() {
@@ -8,7 +8,23 @@ public class First_Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("ENTER THE NUMBER");
+		Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        
+        if(n == 1) {
+            System.out.println("NO");
+            return;
+        }
+        
+        for(int i=2;i<n;i++) {
+            if(n % i == 0) {
+                System.out.println("NO");
+                return;
+            }
+        }
+        
+        System.out.println("YES");
 	}
 
 }
