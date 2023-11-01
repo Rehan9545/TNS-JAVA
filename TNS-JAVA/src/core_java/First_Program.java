26bcc8abee2ba5c846abfb3dc5c1cb7d82343fd6
rehan <rehanfga@gmail.com>
2023-11-01 17:58:56 +0530

    first commit

---
+++ TNS-JAVA/src/core_java/First_Program.java
@@ -0,0 +1,14 @@
+package core_java;
+
+public class First_Program {
+
+	public First_Program() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
